1988068937ad2cb3cd64490cb88e7a7de957dcbc
Stanislav Georgiev <sgeorgiev@mm-sol.com>
2019-11-04 18:34:21 +0200

    tmp100/: Read temperature from /sys/tmp100/tmp100_val. Minor changes in Makefile.

--- tmp100/tmp100.c
+++ tmp100/tmp100.c
@@ -6,6 +6,16 @@
 
 #define BYTES_TO_READ 4
 #define TMP100_TEMP_REG 0x00
+#define ONLY_READ_PERMISSIONS 0444
+
+static ssize_t tmp100_show(struct kobject *kobj,
+		struct kobj_attribute *attr, char *buf);
+
+static struct kobject *kobj_ref;
+static struct kobj_attribute etx_attr = __ATTR(tmp100_val,
+		ONLY_READ_PERMISSIONS, tmp100_show, NULL);
+
+struct i2c_client *master_client;
 
 static s32 tmp100_i2c_read(struct i2c_client *client, u8 reg)
 {
@@ -19,13 +29,20 @@ static s32 tmp100_i2c_read(struct i2c_client *client, u8 reg)
 	}
 }
 
+static ssize_t tmp100_show(struct kobject *kobj, struct kobj_attribute *attr,
+		char *buf)
+{
+	return sprintf(buf, "%d\n", tmp100_i2c_read(master_client, TMP100_TEMP_REG));
+}
+
 static int tmp100_i2c_probe(struct i2c_client *client,
-				const struct i2c_device_id *id)
+		const struct i2c_device_id *id)
 {
 	int error;
 	u8 val[BYTES_TO_READ];
 
 	struct i2c_adapter *adapter = client->adapter;
+	master_client = client;
 
 	error = i2c_check_functionality(adapter, I2C_FUNC_I2C);
 	if (error < 0) {
@@ -37,16 +54,18 @@ static int tmp100_i2c_probe(struct i2c_client *client,
 		return -ENODEV;
 	}
 
-	printk(KERN_INFO "WORD DATA: %d\n", 
-					tmp100_i2c_read(client, TMP100_TEMP_REG));
+	kobj_ref = kobject_create_and_add("tmp100", NULL);
+
+	if(sysfs_create_file(kobj_ref,&etx_attr.attr)){
+		printk(KERN_INFO "Cannot create sysfs file......\n");
+	}
 
 	return 0;
 }
 
 static int tmp100_i2c_remove(struct i2c_client *client)
 {
-	printk(KERN_INFO "tmp100_i2c_driver REMOVE!\n");
-
+	kobject_put(kobj_ref);
 	return 0;
 }
 
@@ -54,14 +73,12 @@ static const struct i2c_device_id tmp100_i2c_id[] = {
 	{ "tmp100", 0 },
 	{} ,
 };
-
 MODULE_DEVICE_TABLE(i2c, tmp100_i2c_id);
 
 static const struct of_device_id tmp100_of_match[] = {
 	{ .compatible = "TI, TMP100" },
 	{},
 };
-
 MODULE_DEVICE_TABLE(of, tmp100_of_match);
 
 static struct i2c_driver tmp100_driver = {
