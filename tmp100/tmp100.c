2e7e4fb6701f8374114f2cd3d47b8af933442e2d
Stanislav Georgiev <sgeorgiev24@gmail.com>
2019-11-04 23:26:28 +0200

    tmp100/tmp100.c: Fix coding style.

--- tmp100/tmp100.c
+++ tmp100/tmp100.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/i2c.h>
@@ -20,19 +21,20 @@ struct i2c_client *master_client;
 static s32 tmp100_i2c_read(struct i2c_client *client, u8 reg)
 {
 	s32 word_data;
+
 	word_data = i2c_smbus_read_byte_data(client, reg);
 
-	if (word_data < 0) {
+	if (word_data < 0)
 		return -EIO;
-	} else {
+	else
 		return word_data;
-	}
 }
 
 static ssize_t tmp100_show(struct kobject *kobj, struct kobj_attribute *attr,
 		char *buf)
 {
-	return sprintf(buf, "%d\n", tmp100_i2c_read(master_client, TMP100_TEMP_REG));
+	return sprintf(buf, "%d\n", tmp100_i2c_read(master_client,
+				TMP100_TEMP_REG));
 }
 
 static int tmp100_i2c_probe(struct i2c_client *client,
@@ -42,23 +44,21 @@ static int tmp100_i2c_probe(struct i2c_client *client,
 	u8 val[BYTES_TO_READ];
 
 	struct i2c_adapter *adapter = client->adapter;
+
 	master_client = client;
 
 	error = i2c_check_functionality(adapter, I2C_FUNC_I2C);
-	if (error < 0) {
+	if (error < 0)
 		return -ENODEV;
-	}
 
 	error = i2c_master_recv(client, val, BYTES_TO_READ);
-	if (error < 0) {
+	if (error < 0)
 		return -ENODEV;
-	}
 
 	kobj_ref = kobject_create_and_add("tmp100", NULL);
 
-	if(sysfs_create_file(kobj_ref,&etx_attr.attr)){
-		printk(KERN_INFO "Cannot create sysfs file......\n");
-	}
+	if (sysfs_create_file(kobj_ref, &etx_attr.attr))
+		pr_info("Cannot create sysfs file!\n");
 
 	return 0;
 }
@@ -71,7 +71,7 @@ static int tmp100_i2c_remove(struct i2c_client *client)
 
 static const struct i2c_device_id tmp100_i2c_id[] = {
 	{ "tmp100", 0 },
-	{} ,
+	{},
 };
 MODULE_DEVICE_TABLE(i2c, tmp100_i2c_id);
 
