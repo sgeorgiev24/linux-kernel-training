7bf463622decd3578cb43ae786ad4f178b82b3c2
Stanislav Georgiev <sgeorgiev@mm-sol.com>
2019-11-11 16:19:05 +0200

    tmp100/tmp100.c: Add char dev to the system and read floating point.

--- tmp100/tmp100.c
+++ tmp100/tmp100.c
@@ -7,19 +7,26 @@
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
 #include <linux/device.h>
+#include <linux/cdev.h>
 
-#define BYTES_TO_READ 4
+#define BYTES_TO_READ 2
 #define TMP100_TEMP_REG 0x00
 #define ONLY_READ_PERMISSIONS 0444
 
-static struct class *dev_class;
-static struct i2c_client *master_client;
-
 dev_t dev;
 
-/*
-static ssize_t tmp100_show(struct kobject *kobj,
-		struct kobj_attribute *attr, char *buf);
+static struct class *dev_class;
+static struct i2c_client *master_client;
+static struct cdev tmp100_cdev;
+
+static ssize_t tmp100_file_read(struct file *filp, char __user *buf,
+		size_t len, loff_t *off);
+
+
+static const struct file_operations fops = {
+	.owner = THIS_MODULE,
+	.read = tmp100_file_read,
+};
 
 static s32 tmp100_i2c_read(struct i2c_client *client, u8 reg)
 {
@@ -33,22 +40,37 @@ static s32 tmp100_i2c_read(struct i2c_client *client, u8 reg)
 		return word_data;
 }
 
-static ssize_t tmp100_show(struct kobject *kobj, struct kobj_attribute *attr,
-		char *buf)
+static ssize_t tmp100_file_read(struct file *filp, char __user *buf,
+		size_t len, loff_t *off)
 {
-	s32 ret;
-	s8 temp;
+	s32 word_data;
+	s16 temp;
+	s8 floating_point;
+	s8 temperature;
 
-	ret = tmp100_i2c_read(master_client, TMP100_TEMP_REG);
+	word_data = tmp100_i2c_read(master_client, TMP100_TEMP_REG);
 
-	if (ret < 0)
-		return ret;
+	if (word_data < 0)
+		return word_data;
 
-	temp = ret;
+	temp = word_data; // 1000 0000 0001 1000 if 8019
+	floating_point = (temp>>12); // 1111 1000 if x.5 or 0000 0000 if x.0
+	floating_point &= 8;
+	temperature = temp & 0xff;
 
-	return sprintf(buf, "%d\n", temp);
+	/*
+	pr_info("TEMP: %d -> %x\n", temp, temp);
+	pr_info("FLOATING POINT: %d -> %x\n", floating_point, floating_point);
+	pr_info("TEMPERATURE: %d -> %x\n", temperature, temperature);
+	*/
+
+	if (floating_point)
+		pr_info("%d.5\n", temperature);
+	else
+		pr_info("%d\n", temperature);
+
+	return 0;
 }
-*/
 
 static int tmp100_i2c_probe(struct i2c_client *client,
 		const struct i2c_device_id *id)
@@ -65,18 +87,38 @@ static int tmp100_i2c_probe(struct i2c_client *client,
 	if (error < 0)
 		return -ENODEV;
 
+	// i2c_master_send(client, val, 1);
 	error = i2c_master_recv(client, val, BYTES_TO_READ);
 	if (error < 0)
 		return -ENODEV;
 
+	/* DEBUG */
+	/*
+	pr_info("VAL: %x\n", val[0]);
+	pr_info("VAL: %x\n", val[1]);
+	pr_info("VAL: %x\n", val[2]);
+	pr_info("VAL: %x\n", val[3]);
+	*/
+	/* DEBUG */
+
 	/* Allocating major number */
 	if (alloc_chrdev_region(&dev, 0, 1, "tmp100_dev")) {
-		pr_err("Connot allocate major number for tmp100\n");
+		pr_err("Cannot allocate major number for tmp100\n");
 		return -1;
 	}
 
+	/* Creating cdev structure */
+	cdev_init(&tmp100_cdev, &fops);
+
+	/* Adding character device to the system */
+	if (cdev_add(&tmp100_cdev, dev, 1) < 0) {
+		pr_info("Cannot add the device to the system.\n");
+		goto r_class;
+	}
+
 	/* Create struct class */
-	if ((dev_class = class_create(THIS_MODULE, "tmp100_class")) == NULL) {
+	dev_class = class_create(THIS_MODULE, "tmp100_class");
+	if (dev_class == NULL) {
 		pr_err("Cannot create the struct class for tmp100\n");
 		goto r_class;
 	}
