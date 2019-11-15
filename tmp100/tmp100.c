4b9c15988a3f8a6c4877f438fdc23356fae4af6f
Stanislav Georgiev <sgeorgiev@mm-sol.com>
2019-11-15 16:52:02 +0200

    tmp100/tmp100.c: Create per-device-structure, print temperature to user correctly.

--- tmp100/tmp100.c
+++ tmp100/tmp100.c
@@ -8,15 +8,19 @@
 #include <linux/fs.h>
 #include <linux/device.h>
 #include <linux/cdev.h>
+#include <linux/uaccess.h>
 
 #define BYTES_TO_READ 2
 #define TMP100_TEMP_REG 0x00
 
-dev_t dev;
+struct tmp100 {
+	dev_t dev;
+	struct class *dev_class;
+	struct i2c_client *master_client;
+	struct cdev tmp100_cdev;
+};
 
-static struct class *dev_class;
-static struct i2c_client *master_client;
-static struct cdev tmp100_cdev;
+struct tmp100 tmp100; // use container_of to get rid of this global var
 
 static ssize_t tmp100_print_temp(struct file *filp, char __user *buf,
 		size_t len, loff_t *off);
@@ -43,24 +47,34 @@ static ssize_t tmp100_print_temp(struct file *filp, char __user *buf,
 		size_t len, loff_t *off)
 {
 	s32 word_data;
-	s32 floating_point;
+	s32 fraction;
 	s8 temperature;
+	char ret[sizeof(temperature) + sizeof(fraction) + 1];
 
-	word_data = tmp100_i2c_read(master_client, TMP100_TEMP_REG);
+	/*
+	 *  word_data format:
+	 * xxxx xxxx xxxx xxxx        xxxx          xxxx          xxxx xxxx
+	 *        \  /                 ||            ||             \   /
+	 *         \/                  \/            \/              \ /
+	 *  zeros if no error       fraction    always zeros     whole number
+	 */
+	word_data = tmp100_i2c_read(tmp100.master_client, TMP100_TEMP_REG);
 
 	if (word_data < 0)
 		return word_data;
 
+	/* we need only the 8 LSB for the whole part of the temperature */
 	temperature = word_data;
 
-	floating_point = word_data>>12;
-	floating_point &= 0xf;
-	floating_point *= 100;
-	floating_point >>= 4;
+	fraction = word_data>>12;
+	fraction &= 0xf;
+	fraction *= 10;
+	fraction >>= 4;
 
-	pr_info("TMP100 temp: %d.%d C\n", temperature, floating_point);
+	if (snprintf(ret, sizeof(ret), "%d.%d\n", temperature, fraction) < 0)
+		return -1;
 
-	return 0;
+	return simple_read_from_buffer(buf, len, off, ret, sizeof(ret));
 }
 
 static int tmp100_i2c_probe(struct i2c_client *client,
@@ -69,8 +83,8 @@ static int tmp100_i2c_probe(struct i2c_client *client,
 	int error;
 	u8 val[BYTES_TO_READ];
 
-	master_client = client;
-	dev = 0;
+	tmp100.master_client = client;
+	tmp100.dev = 0;
 
 	error = i2c_master_recv(client, val, BYTES_TO_READ);
 	if (error < 0)
@@ -82,29 +96,29 @@ static int tmp100_i2c_probe(struct i2c_client *client,
 	}
 
 	/* Allocating major number */
-	if (alloc_chrdev_region(&dev, 0, 1, "tmp100")) {
+	if (alloc_chrdev_region(&tmp100.dev, 0, 1, "tmp100")) {
 		pr_err("Cannot allocate major number for tmp100\n");
 		return -1;
 	}
 
 	/* Creating cdev structure */
-	cdev_init(&tmp100_cdev, &fops);
+	cdev_init(&tmp100.tmp100_cdev, &fops);
 
 	/* Adding character device to the system */
-	if (cdev_add(&tmp100_cdev, dev, 1) < 0) {
+	if (cdev_add(&tmp100.tmp100_cdev, tmp100.dev, 1) < 0) {
 		pr_info("Cannot add the device to the system.\n");
 		goto r_class;
 	}
 
 	/* Create struct class */
-	dev_class = class_create(THIS_MODULE, "tmp100");
-	if (dev_class == NULL) {
+	tmp100.dev_class = class_create(THIS_MODULE, "tmp100");
+	if (tmp100.dev_class == NULL) {
 		pr_err("Cannot create the struct class for tmp100\n");
 		goto r_class;
 	}
 
 	/* Creating the device */
-	if (device_create(dev_class, NULL, dev, NULL, "tmp100") == NULL) {
+	if (device_create(tmp100.dev_class, NULL, tmp100.dev, NULL, "tmp100") == NULL) {
 		pr_err("Cannot create the DEvice tmp100\n");
 		goto r_device;
 	}
@@ -112,17 +126,17 @@ static int tmp100_i2c_probe(struct i2c_client *client,
 	return 0;
 
 r_device:
-	class_destroy(dev_class);
+	class_destroy(tmp100.dev_class);
 r_class:
-	unregister_chrdev_region(dev, 1);
+	unregister_chrdev_region(tmp100.dev, 1);
 	return -1;
 }
 
 static int tmp100_i2c_remove(struct i2c_client *client)
 {
-	device_destroy(dev_class, dev);
-	class_destroy(dev_class);
-	unregister_chrdev_region(dev, 1);
+	device_destroy(tmp100.dev_class, tmp100.dev);
+	class_destroy(tmp100.dev_class);
+	unregister_chrdev_region(tmp100.dev, 1);
 
 	return 0;
 }
